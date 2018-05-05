4240be7087f4a975acb6b19462902144caa378ef
Boris Capitanu <borice@hotmail.com>
2018-05-04 19:37:03 -0500

    Renamed text processing option for clarity

--- src/main/scala/org/hathitrust/htrc/data/TextOptions.scala
+++ src/main/scala/org/hathitrust/htrc/data/TextOptions.scala
@@ -2,5 +2,5 @@ package org.hathitrust.htrc.data
 
 object TextOptions extends Enumeration {
   type TextOptions = Value
-  val TrimLines, RemoveEmptyLines, FixHyphenation, ParaLines = Value
+  val TrimLines, RemoveEmptyLines, DehyphenateAtEol, ParaLines = Value
 }
